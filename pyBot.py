5bfdeea0f4f5981b12e358c4fe80c07ce0d254de
OctoBirb <53840213+OctoBirb@users.noreply.github.com>
2019-09-05 18:31:08 -0400

    v0.00.1
    1st step to the bot part!

--- pyBot.py
+++ pyBot.py
@@ -13,7 +13,12 @@ os.system("cls")
 print("https://www.youtube.com/watch?v=dQw4w9WgXcQ") # You just got rickrolled!
 time.sleep(0.07)
 os.system("cls")
-print("OS: MS Windows 10.0.17134.950")
+print("OS: MS Windows 10.0.17134.950") # If you're on a different type of OS, then... well, i guess you're on Win10.
 print("Version running: " + pyBot[1] + " " + pyBot[2])
 print("Booting up " + pyBot[0] + "...")
 time.sleep(3)
+os.system("cls")
+print("...")
+time.sleep(4)
+os.system("cls")
+print("> Hello!")
