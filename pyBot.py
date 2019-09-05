941aa70b9df9fdcfefb5de623ca1b0d83f6a0e01
OctoBirb <53840213+OctoBirb@users.noreply.github.com>
2019-09-05 18:21:52 -0400

    v0.00
    1st release! Made it boot up, now for the actual bot part.

---
+++ pyBot.py
@@ -0,0 +1,19 @@
+pyBot = ["pyBot", "Alpha", "0.0.1"]
+on = False
+off = True
+startupmode = True
+import time
+import os
+os.system("cls")
+print("OS: MS Windows 10.0.17134.950")
+print("Version running: " + pyBot[1] + " " + pyBot[2])
+print("Creating and importing variables for " + pyBot[0] + "...")
+time.sleep(3)
+os.system("cls")
+print("https://www.youtube.com/watch?v=dQw4w9WgXcQ") # You just got rickrolled!
+time.sleep(0.07)
+os.system("cls")
+print("OS: MS Windows 10.0.17134.950")
+print("Version running: " + pyBot[1] + " " + pyBot[2])
+print("Booting up " + pyBot[0] + "...")
+time.sleep(3)
